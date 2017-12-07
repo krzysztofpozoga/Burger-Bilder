6f4c0e0df82a2a3dd9a9959b2854db35e89b2aec
krzysztofpozoga <pozoga.krzysztof@gmail.com>
2017-12-07 13:08:35 +0100

    Control component - added

---
+++ js/components/Burger/BuildControls/BuildControls.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class BuildControls extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default BuildControls;
---
+++ js/components/Burger/BuildControls/Control.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Control extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className='control'>
+        <div className='label'>{this.props.label}</div>
+        <button className='more'>Więcej!</button>
+        <button className='less'>Mniej!</button>
+      </div>
+    )
+  }
+}
+
+export default Control;
